e5de604a1cbc77f29cab7508882b7f82a7fd9082
Marshall Serlin <marshallserlin@Marshalls-MacBook-Pro.local>
2024-10-31 18:31:02 -0700

    remove unused import

--- src/app/send-events/page.tsx
+++ src/app/send-events/page.tsx
@@ -5,7 +5,7 @@ import { v4 as uuidv4 } from 'uuid'
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 
 type Property = {
   key: string
