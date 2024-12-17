7a25d0456fa1e24633f5d92e89476419ae4f3c1d
Marshall Serlin <marshallserlin@Marshalls-MBP.attlocal.net>
2024-12-16 16:57:55 -0800

    remove unused useEffect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { v4 as uuidv4 } from 'uuid'
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
